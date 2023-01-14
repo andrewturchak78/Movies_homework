6f7b7a22d3f4a443c9fedef58769a1a0b3f6c554
Andrei <andrewturchak78@mail.ru>
2023-01-14 14:49:44 +0300

    findLast tests added

--- src/test/java/org/example/MoviePosterRepositoryTest.java
+++ src/test/java/org/example/MoviePosterRepositoryTest.java
@@ -9,6 +9,11 @@ public class MoviePosterRepositoryTest {
     MoviePoster poster3 = new MoviePoster("Отель Белград");
     MoviePoster poster4 = new MoviePoster("Джентельмены");
     MoviePoster poster5 = new MoviePoster("Человек-невидимка");
+    MoviePoster poster6 = new MoviePoster("Форсаж");
+    MoviePoster poster7 = new MoviePoster("Трансформеры");
+    MoviePoster poster8 = new MoviePoster("Гарри Поттер");
+    MoviePoster poster9 = new MoviePoster("Титаник");
+    MoviePoster poster10 = new MoviePoster("Майор Пейн");
 
     @Test
 
@@ -83,6 +88,46 @@ public class MoviePosterRepositoryTest {
         repo.addFilm(poster5);
         MoviePoster[] actual = repo.findLast();
         MoviePoster[] expected = {poster5, poster4, poster3, poster2, poster1};
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldFindLast6() {
+        MoviePosterRepository repo = new MoviePosterRepository();
+        repo.getLimitOfFilms();
+        repo.addFilm(poster1);
+        repo.addFilm(poster2);
+        repo.addFilm(poster3);
+        repo.addFilm(poster4);
+        repo.addFilm(poster5);
+        repo.addFilm(poster6);
+        repo.addFilm(poster7);
+        repo.addFilm(poster8);
+        repo.addFilm(poster9);
+        repo.addFilm(poster10);
+        MoviePoster[] actual = repo.findLast();
+        MoviePoster[] expected = {poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1};
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldFindLast7() {
+        MoviePosterRepository repo = new MoviePosterRepository();
+        repo.setLimitOfFilms(5);
+        repo.getLimitOfFilms();
+
+        repo.addFilm(poster1);
+        repo.addFilm(poster2);
+        repo.addFilm(poster3);
+        repo.addFilm(poster4);
+        repo.addFilm(poster5);
+        repo.addFilm(poster6);
+        repo.addFilm(poster7);
+        repo.addFilm(poster8);
+        repo.addFilm(poster9);
+        repo.addFilm(poster10);
+        MoviePoster[] actual = repo.findLast();
+        MoviePoster[] expected = {poster10, poster9, poster8, poster7, poster6};
         Assertions.assertArrayEquals(actual, expected);
     }
 }
